b68c7ae7137a815543ba72cacf82ba25e39bb1af
sergey.sokolov <sergey.sokolov@behavox.com>
2017-08-31 19:36:44 +0300

    Updated webpack. Added local css.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ require('jquery-ui/themes/base/core.css');
 require('jquery-ui/themes/base/slider.css');
 require('jquery-ui/themes/base/resizable.css');
 
+require('jquery-ui/ui/widgets/resizable');
+
+require('./index.css');
+
 console.log('test');
